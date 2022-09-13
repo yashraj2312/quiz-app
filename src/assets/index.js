ea8910ff926552f8b22f202a38858090d7f9f357
yashraj2312 <yashrajsrivastava2312@gmail.com>
2022-09-13 23:29:04 +0530

    style:asset modified

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,3 @@
-export { default as homepageimage } from "./homepageimage.svg";
+export { default as homepageimage } from "./result.svg";
 export {default as productOp} from "./productop.png"
 export {default as techOp} from "./tech.jpeg"
